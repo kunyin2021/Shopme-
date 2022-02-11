17acfdadb1250821bc71e36507bc962ea69c9c55
kunyin888 <kunyin888@kunyin888s-iMac.local>
2022-02-10 17:07:03 -0800

    add Customer Registration function and test :wq

--- ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/setting/SettingRepositoryTests.java
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/setting/SettingRepositoryTests.java
@@ -11,7 +11,6 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
 
-import com.shopme.common.entity.Currency;
 import com.shopme.common.entity.Setting;
 import com.shopme.common.entity.SettingCategory;
 
---
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/customer/CustomerRestControoler.java
@@ -0,0 +1,17 @@
+package com.shopme.customer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerRestControoler {
+
+	@Autowired private CustomerService service;
+	
+	@PostMapping("/customers/check_unique_email")
+	public String checkDuplicatEmail(@Param("email") String email) {
+		return service.isEmailUnique(email) ? "OK" : "Duplicated";
+	}
+}
